e90476cd141d5926ea54b7982cb62353fba95d4c
Mohak Singh <singhmohak999@gmail.com>
2026-03-30 03:45:34 +0530

    react-native-recurrly

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+import {Link} from "expo-router";
+
+const SignUp = () => {
+    return (
+        <View>
+            <Text>SignIn</Text>
+            <Link href="/(auth)/sign-in">Sign in</Link>
+        </View>
+    )
+}
+export default SignUp
