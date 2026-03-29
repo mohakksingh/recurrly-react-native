2685fae5ff9178e06b371b379370d6c7195ac05a
Mohak Singh <singhmohak999@gmail.com>
2026-03-30 04:41:53 +0530

    Implemented fixes

--- constants/data.ts
+++ constants/data.ts
@@ -3,6 +3,6 @@ import {icons} from "./icons";
 export const tabs=[
     {name:'index',title:"Home",icon:icons.home},
     {name:'subscriptions',title:"Subscriptions",icon:icons.wallet},
-    {name:'insights',title:"Home",icon:icons.activity},
+    {name:'insights',title:"Insights",icon:icons.activity},
     {name:'settings',title:"Settings",icon:icons.setting},
 ]
