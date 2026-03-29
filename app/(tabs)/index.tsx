c01ca90966a640b1236d66d23ee6e16907779290
Mohak Singh <singhmohak999@gmail.com>
2026-03-30 04:31:20 +0530

    feat:project setup, nativewind,routing, tab navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
 import "@/global.css"
 import { Text, View } from "react-native";
 import {Link} from "expo-router";
+import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+import {styled} from "nativewind";
+
+const SafeAreaView=styled(RNSafeAreaView)
 
 export default function App() {
     return (
-        <View className="flex-1 items-center justify-center bg-background">
+        <SafeAreaView className="flex-1 p-5 bg-background">
             <Text className="text-xl font-bold text-success">
                 Welcome to Nativewind!
             </Text>
@@ -17,6 +21,6 @@ export default function App() {
                 pathname:"/subscriptions/[id]",
                 params:{id:"claude"},
             }} className="mt-4 rounded bg-primary text-white p-4">Claude Max Subscription</Link>
-        </View>
+        </SafeAreaView>
     );
-}
+} 
--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,11 +1,15 @@
 import {View, Text} from 'react-native'
 import React from 'react'
+import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+import {styled} from "nativewind";
+
+const SafeAreaView=styled(RNSafeAreaView)
 
 const Insights = () => {
     return (
-        <View>
+        <SafeAreaView>
             <Text>Insights</Text>
-        </View>
+        </SafeAreaView>
     )
 }
 export default Insights
